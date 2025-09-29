9b3334bdd148e9f02f0669414d1ab8cdf817c62f
Alexei Zakharov <alexeiz@gmail.com>
2025-09-29 00:07:02 -0400

    update dependencies

--- cmake/deps-test.cmake
+++ cmake/deps-test.cmake
@@ -5,6 +5,6 @@ list(APPEND TEST_LIBRARIES Catch2::Catch2WithMain)
 include(cmake/get_cpm.cmake)
 
 CPMAddPackage(
-    URI "gh:alexeiz/scope-exit@0.2.2"
+    URI "gh:alexeiz/scope-exit@0.2.3"
     OPTIONS "BUILD_TESTING OFF")
 list(APPEND TEST_LIBRARIES scope_exit)
