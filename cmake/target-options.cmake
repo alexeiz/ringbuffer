e09bfc79bed27c4718a6fa74b99e6caf032effff
Alexei Zakharov <alexeiz@gmail.com>
2026-03-25 04:05:13 -0400

    refactor centralize compiler options in target-options.cmake
    - add cmake/target-options.cmake with apply_project_options() function
    - include GCC-only flags (-Wduplicated-cond, -Wlogical-op, -Wrestrict)
    - add GCC 14+ flag (-fdiagnostics-all-candidates) via generator expression
    - add MSVC flag (/Zc:preprocessor) for future portability
    - replace ad-hoc compile options in test/CMakeLists.txt with apply_project_options()
    - add outputOnFailure, noTestsAction, and stopOnFailure to test presets

---
+++ cmake/target-options.cmake
@@ -0,0 +1,61 @@
+function(apply_project_options target scope)
+  target_compile_features(${target} ${scope} cxx_std_23)
+  set_target_properties(${target} PROPERTIES
+    CXX_STANDARD 23
+    CXX_STANDARD_REQUIRED ON
+    CXX_EXTENSIONS OFF
+  )
+
+  set(common_options
+    -Wall
+    -Wextra
+    -Werror
+    -Wno-implicit-fallthrough
+    -Wno-unused-parameter
+    -Wunused-variable
+    -Wno-missing-field-initializers
+    -Wnull-dereference
+    -ffast-math
+  )
+
+  foreach(option IN LISTS common_options)
+    target_compile_options(${target} ${scope}
+      $<$<CXX_COMPILER_ID:GNU>:${option}>
+      $<$<CXX_COMPILER_ID:Clang>:${option}>
+    )
+  endforeach()
+
+  set(gcc_only_options
+    -Wduplicated-cond
+    -Wlogical-op
+    -Wrestrict
+    -finput-charset=UTF-8
+    -fexec-charset=UTF-8
+  )
+
+  foreach(option IN LISTS gcc_only_options)
+    target_compile_options(${target} ${scope}
+      $<$<CXX_COMPILER_ID:GNU>:${option}>
+    )
+  endforeach()
+
+  set(gcc_14_options
+    -fdiagnostics-all-candidates
+  )
+
+  foreach(option IN LISTS gcc_14_options)
+    target_compile_options(${target} ${scope}
+      $<$<AND:$<CXX_COMPILER_ID:GNU>,$<VERSION_GREATER_EQUAL:$<CXX_COMPILER_VERSION>,14>>:${option}>
+    )
+  endforeach()
+
+  set(msvc_options
+    /Zc:preprocessor
+  )
+
+  foreach(option IN LISTS msvc_options)
+    target_compile_options(${target} ${scope}
+      $<$<CXX_COMPILER_ID:MSVC>:${option}>
+    )
+  endforeach()
+endfunction()
